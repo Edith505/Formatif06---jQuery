c923bab0fb579dc4b1849ec5d74a405fce64a56c
2433177 <edithrand505@gmail.com>
2025-10-30 09:48:25 -0400

    Start JQuery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    //selection des éléments
+    $('#fullnameHelp').hide();
+    $('#emailHelp').hide();
+    $('#levelHelp').hide();
+    $('#langageHelp').hide();
 
+
+    //erreur de saisi nom
+    $('#fullname').on('input', function () {
+        const nomSaisi = $(this).val().trim();
+        if (nomSaisi.length < 2) {
+            $('#fullnameHelp').show();
+        } else {
+            $('#fullnameHelp').hide();
+        }
+    })
+
+    //erreur de saisi nom
+    $('#email').on('input', function() {
+        const email = $(this).val().trim();
+        if (!isValidEmail(email)) {
+            $('#emailHelp').show();
+        } else {
+            $('#emailHelp').hide();
+        }
+    });
+
+    //fonction Validation email
+    function isValidEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
 });
